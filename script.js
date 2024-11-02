c996f503eb8cd30f26d32b19b48b875052acf842
MD. SHAKHAWAT HOSSAIN <inform.shakhawat@gmail.com>
2024-11-02 11:54:41 +0600

    First Commit

---
+++ script.js
@@ -0,0 +1,80 @@
+// form
+let playerformone = document.querySelector(".playerformone");
+let playerformtwo = document.querySelector(".playerformtwo");
+
+// form buttton/submit
+let playeronebtn = document.querySelector(".playeronebtn");
+let playertwobtn = document.querySelector(".playetwobtn");
+
+// form input
+let playeroneinput = document.querySelector(".playeroneinput");
+let playertwoinput = document.querySelector(".playertwoinput");
+
+// input status
+let inputstatusone = document.querySelector(".statusone");
+let inputstatustwo = document.querySelector(".statustwo");
+
+// form navigationbtn
+const playerone = document.querySelector(".playerone");
+const playertwo = document.querySelector(".playertwo");
+// video
+let video = document.querySelector(".video");
+video.style.display = "none";
+
+// win status
+let winstatus = document.querySelector(".winstatus");
+
+playeronebtn.addEventListener("click", () => {
+  if (!playeroneinput.value) {
+    inputstatusone.innerHTML = "Please input any number.";
+    inputstatusone.style.color = "#ff0000";
+    inputstatusone.style.fontSize = "14px";
+  }
+  if (playeroneinput.value) {
+    inputstatusone.style.display = "none";
+  }
+  if (
+    playeroneinput.value &&
+    playeroneinput.value >= 1 &&
+    playeroneinput.value < 10
+  ) {
+    playerformone.style.left = "-110%";
+    playerone.classList.remove("active");
+    playertwo.classList.add("active");
+    playerformtwo.style.left = "0";
+  }
+});
+
+playertwobtn.addEventListener("click", () => {
+  if (!playertwoinput.value) {
+    inputstatustwo.innerHTML = "Please input any number.";
+    inputstatustwo.style.color = "#ff0000";
+    inputstatustwo.style.fontSize = "14px";
+  }
+  if (
+    playertwoinput.value &&
+    playertwoinput.value >= 1 &&
+    playertwoinput.value < 10 &&
+    playertwoinput.value == playeroneinput.value
+  ) {
+    video.style.display = "block";
+    winstatus.innerHTML = "Player two";
+    playerformtwo.style.display = "none";
+    playerformone.style.display = "none";
+    playerone.classList.remove("active");
+    playertwo.classList.add("active");
+  }
+  if (
+    playertwoinput.value &&
+    playertwoinput.value >= 1 &&
+    playertwoinput.value < 10 &&
+    playertwoinput.value !== playeroneinput.value
+  ) {
+    video.style.display = "block";
+    winstatus.innerHTML = "Player one";
+    playerformtwo.style.display = "none";
+    playerformone.style.display = "none";
+    playerone.classList.add("active");
+    playertwo.classList.remove("active");
+  }
+});
