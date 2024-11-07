c761ab7aa8fb31243c4c77f81320cc1b542b1910
MD. SHAKHAWAT HOSSAIN <inform.shakhawat@gmail.com>
2024-11-07 10:12:31 +0600

    trying number limited

--- script.js
+++ script.js
@@ -25,14 +25,12 @@ video.style.display = "none";
 let winstatus = document.querySelector(".winstatus");
 
 playeronebtn.addEventListener("click", () => {
-  if (!playeroneinput.value || playeroneinput.value == 0) {
+  if (!playeroneinput.value || playeroneinput.value <= 0 || playeroneinput.value >=10) {
     inputstatusone.innerHTML = "Input must be 1-9";
     inputstatusone.style.color = "#ff0000";
     inputstatusone.style.fontSize = "14px";
   }
-  if (playeroneinput.value) {
-    inputstatusone.style.display = "none";
-  }
+
   if (
     playeroneinput.value &&
     playeroneinput.value >= 1 &&
@@ -45,36 +43,38 @@ playeronebtn.addEventListener("click", () => {
   }
 });
 
+let click = 4;
 playertwobtn.addEventListener("click", () => {
-  if (!playertwoinput.value || playertwoinput.value == 0) {
-    inputstatustwo.innerHTML = "Input must be 1-9";
-    inputstatustwo.style.color = "#ff0000";
-    inputstatustwo.style.fontSize = "14px";
+  if(!playertwoinput.value || playertwoinput.value <= 0 || playertwoinput.value >= 10){
+    inputstatustwo.style.color = 'red';
+    inputstatustwo.innerHTML = 'please enter number 1-9';
   }
-  if (
-    playertwoinput.value &&
-    playertwoinput.value >= 1 &&
-    playertwoinput.value < 10 &&
-    playertwoinput.value == playeroneinput.value
-  ) {
-    video.style.display = "block";
-    winstatus.innerHTML = "Player two";
-    playerformtwo.style.display = "none";
-    playerformone.style.display = "none";
-    playerone.classList.remove("active");
-    playertwo.classList.add("active");
-  }
-  if (
-    playertwoinput.value &&
-    playertwoinput.value >= 1 &&
-    playertwoinput.value < 10 &&
-    playertwoinput.value !== playeroneinput.value
-  ) {
-    video.style.display = "block";
-    winstatus.innerHTML = "Player one";
-    playerformtwo.style.display = "none";
-    playerformone.style.display = "none";
-    playerone.classList.add("active");
-    playertwo.classList.remove("active");
+  else{
+    if(playertwoinput.value && playertwoinput.value > 0 && playertwoinput.value < 10 && playeroneinput.value != playertwoinput.value){
+      inputstatustwo.style.color = 'red';
+      click -= 1;
+      inputstatustwo.innerHTML = `Try again , You can try ${click} times`;
+    }
+    
+    if(playertwoinput.value && playertwoinput.value > 0 && playertwoinput.value < 10 && playeroneinput.value != playertwoinput.value && click == 0){
+      playerformtwo.style.display = "none";
+      playerformone.style.display = "none";
+      winstatus.innerHTML = "Player one";
+      video.style.display = "block";
+      playerone.classList.add("active");
+      playertwo.classList.remove("active");
+    }
+    if(playertwoinput.value && playertwoinput.value > 0 && playertwoinput.value < 10 && playeroneinput.value == playertwoinput.value){
+      playerformtwo.style.display = "none";
+      playerformone.style.display = "none";
+      video.style.display = "block";
+      winstatus.innerHTML = "Player two";
+      playerone.classList.remove("active");
+      playertwo.classList.add("active");
+    }
   }
 });
+
+document.querySelector('.reset').addEventListener('click' , ()=>{
+  location.reload();
+})
